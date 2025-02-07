4fa02d1758916792f3942dd592cbb906ac62b861
Mahmoudashraf213 <mhmwdbhjt307@gmail.com>
2025-02-07 13:01:54 +0200

    comment out bootStrap import and invocation in Index.js

--- Index.js
+++ Index.js
@@ -2,14 +2,14 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import { dbConnection } from './db/connection.js';
-import { bootStrap } from './src/bootStrap.js';
+// import { bootStrap } from './src/bootStrap.js';
 const app = express()
 const port = process.env.PORT || 3000;
 dotenv.config({ path: path.resolve("./config/.env") })
 // db connection
 dbConnection()
 // api 
-bootStrap(app, express)
+// bootStrap(app, express)
 
 
 
