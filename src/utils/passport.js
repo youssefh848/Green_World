c115ed9412858d5bfb1537031edc0b7e658f70ce
Youssef <youssefhossam205@gmail.com>
2025-02-18 22:02:03 +0200

    .

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
+            callbackURL: "https://green-world-vert.vercel.app/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
